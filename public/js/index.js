c6d8614c0bb0953d245edc25fcb22cea56991cdc
Terry275 <116662781+Terry275@users.noreply.github.com>
2023-12-17 22:30:23 +0700

    Add more pages

--- public/js/index.js
+++ public/js/index.js
@@ -47,3 +47,86 @@ form.addEventListener("submit", function (e) {
     }
 });
 
+
+
+
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imageResult')
+                .attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(function () {
+    $('#upload').on('change', function () {
+        readURL(input);
+    });
+});
+
+/*  ==========================================
+    SHOW UPLOADED IMAGE NAME
+* ========================================== */
+var input = document.getElementById( 'upload' );
+var infoArea = document.getElementById( 'upload-label' );
+
+input.addEventListener( 'change', showFileName );
+function showFileName( event ) {
+  var input = event.srcElement;
+  var fileName = input.files[0].name;
+  infoArea.textContent = 'File name: ' + fileName;
+}
+
+
+
+/*
+
+
+ _____    _ _ _                                                                       _   
+| ____|__| (_) |_    __ _ _ __  _ __   ___  _   _ _ __   ___ ___ _ __ ___   ___ _ __ | |_ 
+|  _| / _` | | __|  / _` | '_ \| '_ \ / _ \| | | | '_ \ / __/ _ \ '_ ` _ \ / _ \ '_ \| __|
+| |__| (_| | | |_  | (_| | | | | | | | (_) | |_| | | | | (_|  __/ | | | | |  __/ | | | |_ 
+|_____\__,_|_|\__|  \__,_|_| |_|_| |_|\___/ \__,_|_| |_|\___\___|_| |_| |_|\___|_| |_|\__|
+
+
+
+*/
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    toggleEdit();
+});
+
+function toggleEdit() {
+    var titleInput = document.getElementById('exampleFormControlInput1');
+    var contentTextarea = document.getElementById('exampleFormControlTextarea1');
+    var editButton = document.querySelector('.btn-primary');
+
+    if (titleInput.readOnly) {
+        // Enable editing
+        titleInput.readOnly = false;
+        contentTextarea.readOnly = false;
+        titleInput.classList.add('editable');
+        contentTextarea.classList.add('editable');
+        editButton.textContent = 'Edit';
+    } else {
+        // Disable editing
+        titleInput.readOnly = true;
+        contentTextarea.readOnly = true;
+        titleInput.classList.remove('editable');
+        contentTextarea.classList.remove('editable');
+        editButton.textContent = 'Edit';
+    }
+}
+
+
+
+
+
