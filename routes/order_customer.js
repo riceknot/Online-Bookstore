82d3a8e9673d02bef6b96c96ba2ec1af67fe8c81
riceknot <118870849+riceknot@users.noreply.github.com>
2024-01-13 20:41:08 +0700

    Add files via upload

--- routes/order_customer.js
+++ routes/order_customer.js
@@ -1,5 +1,59 @@
 const router = require('express').Router();
 let Order = require('../models/order_model');
 let Account = require('../models/account_model');
+let Inventory = require('../models/inventory_model');
+
+//Render the Order (Customer) page.
+router.route('/').get(async (req, res) => {
+    try {
+        const orders = await Order.find({ status: 'Pending' }).sort({ createdAt: -1 }); //Filter only 'Pending' orders.
+        const customer = await Account.findById(req.userID);
+
+        res.render('customer/order', { orders, customer });
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+//Render the Order History (Customer) page.
+router.route('/history').get(async (req, res) => {
+    try {
+        const orders = await Order.find({ status: { $ne: 'Pending' } }).sort({ createdAt: -1 }); //Filter orders without the status 'Pending'
+        const customer = await Account.findById(req.userID);
+
+        res.render('customer/order-history', { orders, customer });
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+//Render the Order Detail (Customer) page.
+router.route('/order_ID').get(async (req, res) => {
+    try {
+        const customer = await Account.findById(req.userID);
+        const order = await Order.findById(req.params.order_ID);
+
+        let templateName = 'customer/order-detail'; //Default template name
+
+        // Change the EJS file to render based on the status of the order.
+        if (order.status === 'Accepted') {
+            templateName += '-accepted';
+        } else if (order.status === 'Cancelled') {
+            templateName += '-cancel';
+        }
+
+        // Change EJS file based on payment type.
+        if (order.payment_type != 'card') {
+            templateName += '2';
+        }
+
+        console.log(templateName);
+
+        res.render(templateName, { order, customer });
+
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 module.exports = router;
--- routes/order_owner.js
+++ routes/order_owner.js
@@ -1,6 +1,96 @@
 const router = require('express').Router();
 let Order = require('../models/order_model');
 let Account = require('../models/account_model');
+let Inventory = require('../models/inventory_model');
 
+//Render the Order (Owner) page.
+router.route('/').get(async (req, res) => {
+    try {
+        const orders = await Order.find({ status: 'Pending' }).sort({ createdAt: -1 }); //Filter only 'Pending' orders.
+        const owner = await Account.findById(req.userID);
+
+        res.render('owner/order', { orders, owner });
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+//Render the Order History (Owner) page.
+router.route('/history').get(async (req, res) => {
+    try {
+        const orders = await Order.find({ status: { $ne: 'Pending' } }).sort({ createdAt: -1 }); //Filter orders without the status 'Pending'
+        const owner = await Account.findById(req.userID);
+
+        res.render('owner/order-history', { orders, owner });
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+//Render the Order Detail (Owner) page.
+router.route('/order_ID').get(async (req, res) => {
+    try {
+        const owner = await Account.findById(req.userID);
+        const order = await Order.findById(req.params.order_ID);
+        const customer = await Account.findById(order.customer_ID);
+
+        let templateName = 'owner/order-detail'; //Default template name
+
+        // Change the EJS file to render based on the status of the order.
+        if (order.status === 'accepted') {
+            templateName += '-accepted';
+        } else if (order.status === 'cancelled') {
+            templateName += '-cancel';
+        }
+
+        // Change EJS file based on payment type.
+        if (order.payment_type != 'card') {
+            templateName += '2';
+        }
+
+        console.log(templateName);
+
+        res.render(templateName, { order, owner, customer });
+
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+//Function to accept orders.
+router.route('/order_ID/accept').post(async (req, res) => {
+    try {
+        const owner = await Account.findById(req.userID);
+
+        const updateOrder = await Order.findByIdAndUpdate(
+            req.params.order_ID,
+            { $set: { status: 'accepted' } }, // Update the 'status' field
+            { new: true }
+        );
+
+        res.redirect(`/${owner.user_role}/${owner.id}/order/${updateOrder.id}`);
+
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+//Function to cancel orders.
+router.route('/order_ID/cancel').post(async (req, res) => {
+    try {
+        const owner = await Account.findById(req.userID);
+
+        const updateOrder = await Order.findByIdAndUpdate(
+            req.params.order_ID,
+            { $set: { status: 'cancelled' } }, // Update the 'status' field
+            { new: true }
+        );
+
+        res.redirect(`/${owner.user_role}/${owner.id}/order/${updateOrder.id}`);
+
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 module.exports = router;
