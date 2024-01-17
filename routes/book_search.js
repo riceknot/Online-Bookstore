20e671ec9b30d29a1fd34f2bd6d22b1a1e55571d
riceknot <118870849+riceknot@users.noreply.github.com>
2024-01-17 15:01:57 +0700

    Add search and filter function for customer main page

--- routes/book_search.js
+++ routes/book_search.js
@@ -3,45 +3,63 @@ let Inventory = require('../models/inventory_model');
 let Account = require('../models/account_model');
 
 //Render the Book Search Page with all books data.
-router.route('/').get(async(req, res) => {
+router.route('/').get(async (req, res) => {
     try {
         const books = await Inventory.find({})
         const customer = await Account.findById(req.userID);
-        res.render("customer/main",{customer,books});
+        res.render("customer/main", { customer, books });
     } catch (err) {
         console.log(err.message);
     }
 });
 
-//Render the Book Search Page with books data based on the Search and Filter options.
+//Render the Inventory Page with filtered book data.
 router.route('/').post(async (req, res) => {
     try {
-        let bookQuery = {};  //An object used to store the search term and all the filter options found through the 'query'. 
-        const filterFields = ['genre', 'author', 'publisher', 'date'];
+        const customer = await Account.findById(req.userID);
 
-        filterFields.forEach(field => {  //Function used to store each filter option with data from query.
-            if (req.query[`${field}Filter`]) {  //Check if query for filters are empty or not.
-                bookQuery[field] = req.query[`${field}Filter`];
+        const getPriceRange = (option) => {
+            switch (option) {
+                case 'range1':
+                    return { $gte: 0, $lte: 10 };
+                case 'range2':
+                    return { $gt: 10, $lte: 20 };
+                case 'range3':
+                    return { $gt: 20, $lte: 30 };
+                default:
+                    return {}; // Default case or empty object if no specific range is selected
             }
-        });
+        };
 
-        if (req.query.searchTerm) { //If search query is not empty.
-            const searchTerm = new RegExp(req.query.search, 'i');  //Use RegExp to make the search term case-insensitive.
-            bookQuery.book_title = searchTerm;  // store the search term inside 'bookQuery' object.
+        let query = {};
+
+        if (req.body.titleQuery) {
+            query.book_title = { $regex: new RegExp(req.body.titleQuery, 'i') };
         }
 
-        const filteredBooks = await Inventory.find(bookQuery);  // Find specific books based on fields in 'bookQuery'.
-
-        if (filteredBooks.length === 0) { // If no books found based on the search criteria.
-            return res.render('bookSearch', { message: 'No books found based on the search criteria.' }); //Render the Inventory Page with the error message.
+        if (req.body.authorQuery !== 'Author') {
+            query.author = req.body.authorQuery;
         }
 
-        res.render('bookSearch', { filteredBooks }); // Render the Inventory Page with the specified books.
-    }
-    catch (error) {
-        console.log(error.message);
+        if (req.body.genreQuery !== 'Genre') {
+            query.genre = req.body.genreQuery;
+        }
+
+        if (req.body.priceQuery !== 'Price') {
+            const priceRangeQuery = getPriceRange(req.body.priceQuery);
+            if (Object.keys(priceRangeQuery).length !== 0) {
+                query.price = priceRangeQuery;
+            }
+        }
+
+        const books = await Inventory.find(query);
+
+        res.render('customer/main', { customer, books });
+    } catch (err) {
+        console.log(err.message);
     }
 });
+
 //Render the Book Deatil Page with book data 
 router.route('/:book_ID').get(async (req, res) => {
     try {
